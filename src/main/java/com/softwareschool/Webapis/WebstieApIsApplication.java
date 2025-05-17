091e6ff1a5c020a9a9e130c1aa3f3a6aeaf124cb
Rexxyyy <zorakiken@proton.me>
2025-05-17 18:57:29 -0500

    initial commit

---
+++ src/main/java/com/softwareschool/Webapis/WebstieApIsApplication.java
@@ -0,0 +1,14 @@
+package com.softwareschool.Webapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebstieApIsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(WebstieApIsApplication.class, args);
+		System.out.println("hello, mewooo");
+	}
+}
---
+++ src/test/java/com/softwareschool/Webapis/WebstieApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareschool.Webapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebstieApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
